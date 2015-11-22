8d87c59de6a27a7dcd974abea101ca0bb8858aa8
blooies <joy28@msn.com>
2015-11-22 09:11:12 -0500

    highlight tag publications publish one

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('Images', function () {
     return Images.find();
 });
+
+Meteor.publish('Highlights', function () {
+    return Highlights.find({}, {sort: {createdAt: -1}, limit: 1});
+});
