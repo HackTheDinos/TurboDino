78cc1e6a856e976821b4922bcdb3f342c0d09090
blooies <joy28@msn.com>
2015-11-22 01:46:13 -0500

    initial landing home page

---
+++ client/templates/application/home.js
@@ -0,0 +1,13 @@
+Template.home.helpers({
+    photoOfTheWeek: function() {
+    // return Highlight.find().sort({_id: -1}).limit(1).blurb;
+    },
+
+})
+
+
+Template.home.events({
+    'click .submit-fossil': function() {
+        Router.go('/submission');
+    }
+})
