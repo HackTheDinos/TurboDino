95d9526fe8bb0df894b37e5932886bdb83b560de
Johnathan Weisner <joweisner@gmail.com>
2015-11-22 09:03:22 -0500

    Add image gallery

---
+++ client/templates/image/image.js
@@ -0,0 +1,17 @@
+Template.image.helpers({
+    status: function() {
+        return this.status;
+    },
+    location: function() {
+        return this.location;
+    },
+    userStory: function() {
+        return this.userStory;
+    },
+    location: function() {
+        return this.location;
+    },
+    officalStory: function() {
+        return this.officalStory;
+    },
+});
