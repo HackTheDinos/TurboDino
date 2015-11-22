374448fcef7ceda7aff5209d4fd832642de56694
blooies <joy28@msn.com>
2015-11-22 09:27:44 -0500

    merging

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,10 @@ Meteor.publish('Images', function () {
     return Images.find();
 });
 
+Meteor.publish('Comments', function () {
+    return Comments.find();
+});
+
 Meteor.publish('Highlights', function () {
     return Highlights.find({}, {sort: {createdAt: -1}, limit: 1});
 });
